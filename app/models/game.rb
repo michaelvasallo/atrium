51c7ce6b674abaf99d8ade7c78ac2d72814488dd
Michael Vasallo <mjrvasallo@gmail.com>
2017-11-01 18:42:47 +0000

    rubocop fixes

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,8 @@
 class Game < ApplicationRecord
-  belongs_to :developer, :class_name => "Company", :foreign_key => :developer_id
-  belongs_to :publisher, :class_name => "Company", :foreign_key => :publisher_id
-  has_many :game_genres
+  belongs_to :developer, class_name: 'Company', foreign_key: :developer_id
+  belongs_to :publisher, class_name: 'Company', foreign_key: :publisher_id
+
+  has_many :game_genres, dependent: :destroy
   has_many :genres, through: :game_genres
 
   validates :title, :description, :price, :release_date, presence: true
