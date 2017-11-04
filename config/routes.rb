a04bc4edbad0e57441a682d882fa99961d6985dc
Michael Vasallo <mjrvasallo@gmail.com>
2017-11-04 01:30:26 +0000

    created games controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'games/', to: 'games#index', as: 'games'
+
+  get 'game/:id', to: 'games#show', as: 'game', id: /\d+/
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
