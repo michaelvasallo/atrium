63ba2e45f2666642b5e0b85f30e1ad5cef12ebdb
Michael Vasallo <mjrvasallo@gmail.com>
2017-12-26 19:45:37 -0600

    Installed rspec-rails

--- test/controllers/orders_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class OrdersControllerTest < ActionDispatch::IntegrationTest
-  test "should get checkout" do
-    get orders_checkout_url
-    assert_response :success
-  end
-
-end
