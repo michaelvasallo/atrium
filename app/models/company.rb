4cccb17b465a11655bfdc9f5dda3497961371a8f
Michael Vasallo <mjrvasallo@gmail.com>
2017-12-26 20:49:27 -0600

    Added Games relation to Company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,17 @@
 class Company < ApplicationRecord
+  has_many :developed_games,
+           class_name: 'Game',
+           foreign_key: :developer_id,
+           dependent: :restrict_with_error
+
+  has_many :published_games,
+           class_name: 'Game',
+           foreign_key: :publisher_id,
+           dependent: :restrict_with_error
+
   validates :name, presence: true, uniqueness: true
+
+  def games
+    developed_games.or(published_games).distinct
+  end
 end
